eab2f863a4df4e7f97d49ed76c9a18e2a26b4e19
Carson Cook <cook.carson38@gmail.com>
2018-06-20 17:39:14 -0400

    created edge case insert/delete exceptions

--- Structures/List/LinkedList.h
+++ Structures/List/LinkedList.h
@@ -12,6 +12,34 @@ public:
     }
 };
 
+class InsertNodeAfterNodeNotInList : public std::exception {
+public:
+    const char *what() const noexcept override {
+        return "Tried to insert a Node before a Node not in the list!";
+    }
+};
+
+class RemoveNodeNotInList : public std::exception {
+public:
+    const char *what() const noexcept override {
+        return "Tried to remove a Node that's not in the list!";
+    }
+};
+
+class RemoveNodeEmptyList : public RemoveNodeNotInList {
+public:
+    const char *what() const noexcept override {
+        return "Tried to remove a Node that's not in the list!";
+    }
+};
+
+class RemoveNodeAfterLast : public RemoveNodeNotInList {
+public:
+    const char *what() const noexcept override {
+        return "Tried to remove a Node after the last node in the list!";
+    }
+};
+
 template<class T, class NodeType>
 class LinkedList {
 protected:
