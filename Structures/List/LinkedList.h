1dcb9f4c07eb467e0a84694226ad6a233140738b
Carson Cook <cook.carson38@gmail.com>
2018-06-20 18:49:27 -0400

    made own class for LL exceptions

--- Structures/List/LinkedList.h
+++ Structures/List/LinkedList.h
@@ -2,41 +2,7 @@
 
 #include <ostream>
 #include "Nodes/Node.h"
-
-class NoValueFoundListException : public std::exception {
-public:
-    const char *what() const noexcept override {
-        return "Tried to find a value that isn't in the list!";
-    }
-};
-
-class InsertNodeAfterNodeNotInList : public std::exception {
-public:
-    const char *what() const noexcept override {
-        return "Tried to insert a Node before a Node not in the list!";
-    }
-};
-
-class RemoveNodeNotInList : public std::exception {
-public:
-    const char *what() const noexcept override {
-        return "Tried to remove a Node that's not in the list!";
-    }
-};
-
-class RemoveNodeEmptyList : public RemoveNodeNotInList {
-public:
-    const char *what() const noexcept override {
-        return "Tried to remove a Node that's not in the list!";
-    }
-};
-
-class RemoveNodeAfterLast : public RemoveNodeNotInList {
-public:
-    const char *what() const noexcept override {
-        return "Tried to remove a Node after the last node in the list!";
-    }
-};
+#include "LinkedListExceptions.h"
 
 template<class T, class NodeType>
 class LinkedList {
